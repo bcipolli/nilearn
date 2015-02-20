eccd686b3dd288d00258345e7d5b307c7cb62db6
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-19 23:21:08 -0800

    Allow plotting of intra-, inter-, rh-only, lh-only connections.
    Conflicts:
    	nilearn/plotting/img_plotting.py

--- examples/connectivity/plot_adhd_covariance_byhemi.py
+++ examples/connectivity/plot_adhd_covariance_byhemi.py
@@ -5,8 +5,8 @@ Computation of covariance matrix between brain regions
 This example shows how to extract signals from regions defined by an atlas,
 and to estimate a covariance matrix based on these signals.
 """
-n_subjects = 10  # Number of subjects to consider for group-sparse covariance
-plotted_subject = 0  # subject to plot
+n_subjects = 20  # Number of subjects to consider for group-sparse covariance
+plotted_subject = 1  # subject to plot
 n_jobs = 1
 
 import numpy as np
@@ -15,20 +15,52 @@ import matplotlib.pyplot as plt
 from sklearn.externals.joblib import Memory
 
 import nilearn
-from nilearn import plotting, image
-from nilearn.input_data.hemisphere_masker import split_bilateral_rois
+from nilearn import plotting
+from nilearn.input_data.hemisphere_masker import HemisphereMasker, split_bilateral_rois
 from nilearn.plotting import cm
+from nilearn.image import index_img, iter_img
 
 
-def plot_connectome(cov, atlas_maps, **kwargs):
-    """Plot connectome given a covariance matrix and atlas maps"""
-    imgs = image.iter_img(atlas_maps)
+def compute_lh_idx(atlas_maps):
+    idx = []
+    lh_masker = HemisphereMasker(hemisphere='L')
+    lh_masker.fit(atlas_maps)
+    for img in iter_img(atlas_maps):
+        idx.append(lh_masker.transform(img).sum() > 0)
+    return np.asarray(idx, dtype=np.bool)
+
+def plot_connectome(cov, atlas_maps, plot_type='all', random_seed=42, **kwargs):
+    """Plot connectome given a covariance matrix and atlas maps
+
+    plot_type in ['all', 'lh', 'rh', 'intra', 'inter']
+
+    assumptions: in atlas_maps, even #s are lh, odd # masks are rh"""
+
+    # Select sub-region of covariance matrix and image
+    lh_idx = compute_lh_idx(atlas_maps)
+    rh_idx = np.logical_not(lh_idx)
+
+    # Choose colors
+    np.random.seed(random_seed)
+    hemi_node_colors = np.random.rand(len(lh_idx) // 2, 3)
+    node_colors = np.empty((len(lh_idx), 3))
+    node_colors[lh_idx, :] = hemi_node_colors
+    node_colors[rh_idx, :] = hemi_node_colors
+
+    if plot_type == 'intra':
+        cov = cov.copy()
+        cov[np.ix_(lh_idx, rh_idx)] = 0
+        cov[np.ix_(rh_idx, lh_idx)] = 0
+    elif plot_type == 'inter':
+        cov = cov.copy()
+        cov[np.ix_(lh_idx, lh_idx)] = 0
+        cov[np.ix_(rh_idx, rh_idx)] = 0
+
+    imgs = iter_img(atlas_maps)
     regions_coords = np.array([
         map(np.asscalar, plotting.find_xyz_cut_coords(img)) for img in imgs])
-    np.random.seed(42)
-    node_colors = np.random.rand(len(regions_coords) // 2, 3)
-    node_colors = np.concatenate([node_colors, node_colors], axis=0)
-    node_colors = np.sort(node_colors, axis=0)
+
+    # Plot
     plotting.plot_connectome(cov, regions_coords,
                              nodes_kwargs={'s': 50, 'c': node_colors},
                              **kwargs)
@@ -107,11 +139,6 @@ for func_filename, confound_filename in zip(func_filenames,
     subjects.append(region_ts)
 
 # Computing group-sparse precision matrices ###################################
-print("-- Computing group-sparse precision matrices ...")
-from nilearn.group_sparse_covariance import GroupSparseCovarianceCV
-gsc = GroupSparseCovarianceCV(n_jobs=n_jobs, verbose=2)
-gsc.fit(subjects)
-
 print("-- Computing graph-lasso precision matrices ...")
 from sklearn import covariance
 gl = covariance.GraphLassoCV(n_jobs=n_jobs, verbose=2)
@@ -119,41 +146,15 @@ gl.fit(subjects[plotted_subject])
 
 # Displaying results ##########################################################
 print("-- Displaying results")
-title = "{0:d} GroupSparseCovariance $\\alpha={1:.2e}$".format(plotted_subject,
-                                                               gsc.alpha_)
 
-plot_connectome(gsc.covariances_[..., plotted_subject],
-                maps_img, edges_threshold='80%',
-                title=title)
-plot_matrices(gsc.covariances_[..., plotted_subject],
-              gsc.precisions_[..., plotted_subject], title)
+cov = gl.covariance_
 
-title = "{0:d} GraphLasso $\\alpha={1:.2e}$".format(plotted_subject,
-                                                    gl.alpha_)
-plot_connectome(gl.covariance_,
-                maps_img, edges_threshold='80%',
-                title=title)
-plot_matrices(gl.covariance_, gl.precision_, title)
-
+fh = plt.figure()
+for ai, plot_type in enumerate(['intra', 'inter', 'all']):
+    ax = fh.add_subplot(3, 1, ai)
+    display = plot_connectome(cov, maps_img,
+                              plot_type=plot_type,
+                              edges_threshold='70%',
+                              title='threshold=70%% [%s]' % plot_type,
+                              axes=ax)
 plt.show()
-
-imgs = list(image.iter_img(maps_img))
-coords = np.array([
-    map(np.asscalar, plotting.find_xyz_cut_coords(img)) for img in imgs])
-
-cov = gsc.covariances_[..., plotted_subject]
-
-from nilearn import plotting
-np.random.seed(42)
-node_colors = np.random.rand(maps_img.shape[-1], 3)
-display = plotting.plot_connectome(cov, coords,
-                                   edges_threshold=0.38,
-                                   title='threshold=0.38',
-                                   nodes_kwargs={
-                                       's': 50, 'c': node_colors})
-
-display = plotting.plot_connectome(cov, coords,
-                                   edges_threshold='70%',
-                                   title='threshold=70%',
-                                   nodes_kwargs={
-                                       's': 50, 'c': node_colors})
