27df2670193906862dd55377bbce452da2ea9aea
Ben Cipollini <bcipolli@ucsd.edu>
2015-02-23 13:34:46 -0800

    Add mosaic plotting.

--- nilearn/plotting/__init__.py
+++ nilearn/plotting/__init__.py
@@ -38,8 +38,8 @@ from . import cm
 from .img_plotting import plot_img, plot_anat, plot_epi, \
     plot_roi, plot_stat_map, plot_glass_brain, plot_connectome
 from .find_cuts import find_xyz_cut_coords
+from .mosaic import plot_in_grid
 
 __all__ = ['cm', 'plot_img', 'plot_anat', 'plot_epi',
            'plot_roi', 'plot_stat_map', 'plot_glass_brain',
-           'plot_connectome',
-           'find_xyz_cut_coords']
+           'plot_connectome', 'plot_in_grid', 'find_xyz_cut_coords']
