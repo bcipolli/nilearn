9cfd3c180b71b8b4f605e33995003943d7ab27e8
Ben Cipollini <bcipolli@ucsd.edu>
2015-12-25 08:15:21 -0800

    RF: turn generate_components into a function.

--- generate_components.py
+++ generate_components.py
@@ -4,7 +4,6 @@
 
 import os
 import os.path as op
-import warnings
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -15,8 +14,6 @@ from nilearn._utils import check_niimg
 from nilearn.plotting import plot_stat_map
 from scipy import stats
 from sklearn.decomposition import FastICA
-warnings.simplefilter('ignore', DeprecationWarning)
-warnings.simplefilter('error', RuntimeWarning)  # Catch numeric issues in imgs
 
 from hemisphere_masker import HemisphereMasker
 
@@ -37,102 +34,119 @@ def cast_img(img, dtype=np.float32):
     return new_img_like(img, img_data)
 
 
-# Get image and term data #####################################################
-# Download 100 matching images
-ss_all = datasets.fetch_neurovault(max_images=np.inf,  # Use np.inf for all.
-                                   map_types=['F map', 'T map', 'Z map'],
-                                   fetch_terms=True)
-images, collections = ss_all['images'], ss_all['collections']
-term_scores = ss_all['terms']
+def generate_components(n_images=np.inf, n_components=20,
+                        hemis=('both', 'R', 'L'), random_state=42):
+    # Get image and term data
 
-# Clean & report term scores
-terms = np.array(term_scores.keys())
-term_matrix = np.asarray(term_scores.values())
-term_matrix[term_matrix < 0] = 0
-total_scores = np.mean(term_matrix, axis=1)
+    # Download 100 matching images
+    ss_all = datasets.fetch_neurovault(max_images=np.inf,  # Use np.inf for all
+                                       map_types=['F map', 'T map', 'Z map'],
+                                       fetch_terms=True)
+    images = ss_all['images']
+    term_scores = ss_all['terms']
 
-print("Top 10 neurosynth terms from downloaded images:")
-for term_idx in np.argsort(total_scores)[-10:][::-1]:
-    print('\t%-25s: %.2f' % (terms[term_idx], total_scores[term_idx]))
+    # Clean & report term scores
+    terms = np.array(term_scores.keys())
+    term_matrix = np.asarray(term_scores.values())
+    term_matrix[term_matrix < 0] = 0
+    total_scores = np.mean(term_matrix, axis=1)
 
-print("Creating a grey matter mask.")
-target_img = datasets.load_mni152_template()
-grey_voxels = (target_img.get_data() > 0).astype(int)
-mask_img = new_img_like(target_img, grey_voxels)
+    print("Top 10 neurosynth terms from downloaded images:")
+    for term_idx in np.argsort(total_scores)[-10:][::-1]:
+        print('\t%-25s: %.2f' % (terms[term_idx], total_scores[term_idx]))
 
-print("Running all analyses on both hemis together, and each separately.")
-for hemi in ['both', 'R', 'L']:
-    # Reshape & mask images ###################################################
-    print("%s: Reshaping and masking images; may take time." % hemi)
-    if hemi == 'both':
-        masker = NiftiMasker(mask_img=mask_img,
-                             target_affine=target_img.affine,
-                             target_shape=target_img.shape,
-                             memory='nilearn_cache')
+    print("Creating a grey matter mask.")
+    target_img = datasets.load_mni152_template()
+    grey_voxels = (target_img.get_data() > 0).astype(int)
+    mask_img = new_img_like(target_img, grey_voxels)
 
-    else:  # R and L maskers
-        masker = HemisphereMasker(mask_img=mask_img,
-                                  target_affine=target_img.affine,
-                                  target_shape=target_img.shape,
-                                  memory='nilearn_cache', hemisphere=hemi)
-    masker = masker.fit()
+    print("Running all analyses on both hemis together, and each separately.")
 
-    # Images may fail to be transformed, and are of different shapes,
-    # so we need to trasnform one-by-one and keep track of failures.
-    X = []
-    xformable_idx = np.ones((len(images),), dtype=bool)
-    for ii, im in enumerate(images):
-        img = cast_img(im['local_path'], dtype=np.float32)
-        img = clean_img(img)
-        try:
-            X.append(masker.transform(img))
-        except Exception as e:
-            print("Failed to mask/reshape image %d/%s: %s" % (
-                im.get('collection_id', 0), op.basename(im['local_path']), e))
-            xformable_idx[ii] = False
+    for hemi in hemis:
+        # Reshape & mask images
+        print("%s: Reshaping and masking images; may take time." % hemi)
+        if hemi == 'both':
+            masker = NiftiMasker(mask_img=mask_img,
+                                 target_affine=target_img.affine,
+                                 target_shape=target_img.shape,
+                                 memory='nilearn_cache')
 
-    # Now reshape list into 2D matrix, and remove failed images from terms
-    X = np.vstack(X)
-    term_matrix = term_matrix[:, xformable_idx]
+        else:  # R and L maskers
+            masker = HemisphereMasker(mask_img=mask_img,
+                                      target_affine=target_img.affine,
+                                      target_shape=target_img.shape,
+                                      memory='nilearn_cache', hemisphere=hemi)
+        masker = masker.fit()
 
-    # Run ICA and map components to terms #####################################
-    print("%s: Running ICA; may take time..." % hemi)
-    fast_ica = FastICA(n_components=20, random_state=42)
-    ica_maps = fast_ica.fit_transform(X.T).T
+        # Images may fail to be transformed, and are of different shapes,
+        # so we need to trasnform one-by-one and keep track of failures.
+        X = []  # noqa
+        xformable_idx = np.ones((len(images),), dtype=bool)
+        for ii, im in enumerate(images):
+            img = cast_img(im['local_path'], dtype=np.float32)
+            img = clean_img(img)
+            try:
+                X.append(masker.transform(img))
+            except Exception as e:
+                print("Failed to mask/reshape image %d/%s: %s" % (
+                    im.get('collection_id', 0),
+                    op.basename(im['local_path']),
+                    e))
+                xformable_idx[ii] = False
 
-    # Don't use the transform method as it centers the data
-    ica_terms = np.dot(term_matrix, fast_ica.components_.T).T
+        # Now reshape list into 2D matrix, and remove failed images from terms
+        X = np.vstack(X)  # noqa
+        term_matrix = term_matrix[:, xformable_idx]
 
-    # Generate figures ########################################################
-    print("%s: Generating figures." % hemi)
+        # Run ICA and map components to terms
+        print("%s: Running ICA; may take time..." % hemi)
+        fast_ica = FastICA(n_components=n_components,
+                           random_state=random_state)
+        ica_maps = fast_ica.fit_transform(X.T).T
 
-    ica_images = masker.inverse_transform(ica_maps)
+        # Don't use the transform method as it centers the data
+        ica_terms = np.dot(term_matrix, fast_ica.components_.T).T
 
-    # Write to disk
-    out_path = op.join('ica_nii', '%s_ica_components.nii.gz' % hemi)
-    if not op.exists(op.dirname(out_path)):
-        os.makedirs(op.dirname(out_path))
-    ica_images.to_filename(out_path)
+        # Generate figures
+        print("%s: Generating figures." % hemi)
 
-    for idx, (ic, ic_terms) in enumerate(zip(ica_maps, ica_terms)):
-        if -ic.min() > ic.max():
-            # Flip the map's sign for prettiness
-            ic = -ic
-            ic_terms = -ic_terms
+        ica_images = masker.inverse_transform(ica_maps)
 
-        ic_thr = stats.scoreatpercentile(np.abs(ic), 90)
-        ic_img = masker.inverse_transform(ic)
-        display = plot_stat_map(ic_img, threshold=ic_thr, colorbar=False,
-                                bg_img=target_img)
-
-        # Use the 4 terms weighted most as a title
-        important_terms = terms[np.argsort(ic_terms)[-4:]]
-        title = '%d: %s' % (idx, ', '.join(important_terms[::-1]))
-        display.title(title, size=16)
-
-        # Save images instead of displaying
-        out_path = op.join('ica_maps', '%s_component_%i.png' % (hemi, idx))
+        # Write to disk
+        out_path = op.join('ica_nii', str(n_components),
+                           '%s_ica_components.nii.gz' % hemi)
         if not op.exists(op.dirname(out_path)):
             os.makedirs(op.dirname(out_path))
-        plt.savefig(out_path)
-        plt.close()
+        ica_images.to_filename(out_path)
+
+        for idx, (ic, ic_terms) in enumerate(zip(ica_maps, ica_terms)):
+            if -ic.min() > ic.max():
+                # Flip the map's sign for prettiness
+                ic = -ic
+                ic_terms = -ic_terms
+
+            ic_thr = stats.scoreatpercentile(np.abs(ic), 90)
+            ic_img = masker.inverse_transform(ic)
+            display = plot_stat_map(ic_img, threshold=ic_thr, colorbar=False,
+                                    bg_img=target_img)
+
+            # Use the 4 terms weighted most as a title
+            important_terms = terms[np.argsort(ic_terms)[-4:]]
+            title = '%d: %s' % (idx, ', '.join(important_terms[::-1]))
+            display.title(title, size=16)
+
+            # Save images instead of displaying
+            out_path = op.join('ica_maps', str(n_components),
+                               '%s_component_%i.png' % (hemi, idx))
+            if not op.exists(op.dirname(out_path)):
+                os.makedirs(op.dirname(out_path))
+            plt.savefig(out_path)
+            plt.close()
+
+
+if __name__ == '__main__':
+    import warnings
+    warnings.simplefilter('ignore', DeprecationWarning)
+    warnings.simplefilter('error', RuntimeWarning)  # Detect bad NV images
+
+    generate_components(n_components=20)
